3ccfa1631d9206bf53ffa1e3be9b26d9cc222389
arav39marri <aravindmarripelli@gmail.com>
2025-12-06 16:25:25 +0530

    Remove secrets from .env and oauth.js

--- auth_server/oauth.js
+++ auth_server/oauth.js
@@ -2,8 +2,8 @@ const passport = require('passport');
 const { stringify } = require('querystring');
 const GoogleStrategy = require('passport-google-oauth2').Strategy;
 
-const GOOGLE_CLIENT_ID ="YOUR_CLIENT_ID"
-const GOOGLE_CLIENT_SECRET ="YOUR_CLIENT_SECRET";
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 
 passport.use(new GoogleStrategy({
   clientID: GOOGLE_CLIENT_ID,
